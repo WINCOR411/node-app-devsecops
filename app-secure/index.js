3390fbebfa346a074310489bc613c97d187aebbd
WINCOR <corentin.faure@afip-formations.com>
2026-05-11 10:37:10 +0200

    feat: display "Hello from Secure App" text

--- app-secure/index.js
+++ app-secure/index.js
@@ -3,12 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello DevSecOps M2 !',
-    app: 'node-app-secure',
-    version: '1.0.0',
-    status: 'Secure - node:18-alpine'
-  });
+  res.send('Hello from Secure App');
 });
 
 app.get('/health', (req, res) => {
